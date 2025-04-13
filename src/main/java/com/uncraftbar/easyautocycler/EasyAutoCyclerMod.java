ecc180526d4b44f392a50b973f034da4249ed9c9
Uncraftbar <joey.crafter13@gmail.com>
2025-04-13 21:17:26 +0200

    Implemented Auto cycling for Enchants, ready for first release.

--- src/main/java/com/uncraftbar/easyautocycler/EasyAutoCyclerMod.java
+++ src/main/java/com/uncraftbar/easyautocycler/EasyAutoCyclerMod.java
@@ -1,30 +1,28 @@
 package com.uncraftbar.easyautocycler;
 
 import com.mojang.logging.LogUtils;
+import com.uncraftbar.easyautocycler.command.CommandSetTrade;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
 import org.slf4j.Logger;
 
-@Mod(EasyAutoCyclerMod.MODID) // MODID must match build.gradle and mods.toml
+
+@Mod(EasyAutoCyclerMod.MODID)
 public class EasyAutoCyclerMod {
 
-    public static final String MODID = "easyautocycler"; // Replace with your mod ID
+    public static final String MODID = "easyautocycler";
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public EasyAutoCyclerMod(IEventBus modEventBus) {
-        // Register common setup
         modEventBus.addListener(this::commonSetup);
-        // Register client setup
         modEventBus.addListener(this::clientSetup);
-
-        // Register other mod-specific event listeners if needed (e.g., config loading)
-        // ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ConfigHandler.SPEC, "easyautocycler-client.toml");
-
-        // Register keybinding event listener
         modEventBus.addListener(Keybindings::registerKeyMappings);
+
+        NeoForge.EVENT_BUS.addListener(this::registerCommands);
 
         LOGGER.info("EasyAutoCyclerMod loaded!");
     }
@@ -35,9 +33,13 @@ public class EasyAutoCyclerMod {
 
     private void clientSetup(final FMLClientSetupEvent event) {
         LOGGER.info("Client setup");
-        // Register client-side event handlers to the Forge event bus
         NeoForge.EVENT_BUS.register(new ClientEventHandler());
-        NeoForge.EVENT_BUS.register(new InputHandler()); // Register input handler for keybinds
+        NeoForge.EVENT_BUS.register(new InputHandler());
         LOGGER.info("Client event handlers registered.");
+    }
+
+    public void registerCommands(RegisterCommandsEvent event) {
+        CommandSetTrade.register(event.getDispatcher(), event.getBuildContext());
+        LOGGER.info("Registered commands.");
     }
 }
